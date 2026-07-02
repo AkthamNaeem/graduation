b4955688cd7347db44dd14c0e9e2329fb732c87c
AkthamNaeem <103374217+AkthamNaeem@users.noreply.github.com>
2026-07-02 17:00:40 +0300

    Update application resource

--- app/Http/Resources/Api/V1/JobApplicationResource.php
+++ app/Http/Resources/Api/V1/JobApplicationResource.php
@@ -5,18 +5,15 @@ namespace App\Http\Resources\Api\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin \App\Models\JobApplication */
 class JobApplicationResource extends JsonResource
 {
-    /**
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'job_posting_id' => $this->job_posting_id,
             'job_seeker_profile_id' => $this->job_seeker_profile_id,
+            'selected_cv_file_id' => $this->selected_cv_file_id,
             'application_status_id' => $this->application_status_id,
             'status' => ApplicationStatusResource::make($this->whenLoaded('applicationStatus')),
             'job_posting' => JobPostingResource::make($this->whenLoaded('jobPosting')),
